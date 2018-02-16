c8d88244e399f1a8e7743efdd4ae49237bca13a2
= <=>
2018-02-16 04:04:23 -0600

    80. Passing Data with Event and Property Binding

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  //selectedRecipe:Recipe = new Recipe('Recipe Name X', 'Description', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg');
+  selectedRecipe:Recipe;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  // onRecipeSelected(selectedRecipe:Recipe) {
+  //   this.selectedRecipe = selectedRecipe;
+  // }
 }
