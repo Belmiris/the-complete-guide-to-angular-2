ff1606bc1377af11fd1c75f8e1149e66e1d76ff1
= <=>
2018-03-11 08:40:27 -0500

    153. Adding Editing Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { InitialRecipeComponent } from './recipes/initial-recipe/initial-recipe.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { InitialRecipeComponent } from './recipes/initial-recipe/initial-recipe.
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    InitialRecipeComponent
+    InitialRecipeComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
